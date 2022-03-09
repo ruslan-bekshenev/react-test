8053c7431ce22ea3984ef8671fbb2335d63b28cb
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-03-10 02:36:58 +0500

    fireEvent, userEvent

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App'
 
 describe('TEST APP', () => {
@@ -11,7 +12,43 @@ describe('TEST APP', () => {
     expect(helloWorld).toBeInTheDocument();
     expect(btn).toBeInTheDocument();
     expect(input).toBeInTheDocument();
-    expect(input).toMatchSnapshot();
+    // expect(input).toMatchSnapshot();
     // screen.debug()
+  })
+
+  test('renders learn react link', async () => {
+    render(<App />)
+    // const helloWorldElement = screen.queryByText(/hello2/i)
+    // expect(helloWorldElement)
+    const helloWorldElement = await screen.findByText(/data/i)
+    expect(helloWorldElement).toBeInTheDocument()
+    expect(helloWorldElement).toHaveStyle({ color: 'red' })
+  })
+
+  test('Click event', () => {
+    render(<App />)
+
+    const btn = screen.getByTestId('toggle-btn')
+    expect(screen.queryByTestId('toggle-elem')).toBeNull()
+    fireEvent.click(btn)
+    expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument()
+    fireEvent.click(btn)
+    expect(screen.queryByTestId('toggle-elem')).toBeNull()
+  })
+
+  test('Input event', () => {
+    render(<App />)
+
+    const input = screen.getByPlaceholderText(/input value/i)
+    // Сверяет содержимое внутри элемента
+    expect(screen.queryByTestId('value-elem')).toContainHTML('')
+    // Позволяет работать с событиями, имитация поведения DOM-элементов
+    // fireEvent.input(input, {
+    //   target: { value: '123123' }
+    // })
+    // Оболочка над fireEvent, имеет более высокий уровень абстракции, эмулирует полное поведение пользователей
+    // Как будто бы это пользователь. Обрабатываются события нажатия клавиш и тд
+    userEvent.type(input, '123123')
+    expect(screen.queryByTestId('value-elem')).toContainHTML('123123')
   })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState<any>(null)
+  const [toggle, setToggle] = useState<any>(false)
+  const [value, setValue] = useState<string>("")
+
+  const handleClick = () => setToggle((prev: boolean) => !prev)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData([])
+    }, 100)
+  }, [])
   return (
     <div>
+      <h1 data-testid="value-elem">{value}</h1>
+      {toggle && <div data-testid="toggle-elem">toggle</div>}
+      {data && <div style={{ color: "red" }}>data</div>}
       <h1>Hello world!</h1>
-      <button>click me</button>
-      <input type="text" placeholder="input value" />
+      <button data-testid="toggle-btn" onClick={handleClick}>click me</button>
+      <input type="text" placeholder="input value" onChange={(e) => setValue(e.target.value)} />
     </div>
   );
 }
